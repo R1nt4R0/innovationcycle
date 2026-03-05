0b578cf1317def94a0af0444ea52cd8deb28acc4
Vladuyinn <romaincrombetravail@gmail.com>
2026-03-05 11:30:28 +0100

    feat: Ajout du header
    Ajout du header ainsi que des couleurs primaires de l'application

---
+++ front-app/src/app/components/header/header.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header { }
